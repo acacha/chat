d0e963981188dcddb7b8aca045c176eabe6a02c4
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-02-28 21:02:38 +0100

    Sync

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -68,15 +68,7 @@
 					</div>
 					<!-- /.box-body -->
 					<div class="box-footer">
-						<form action="#" method="post">
-							{{ csrf_field() }}
-							<div class="input-group">
-								<input type="text" name="message" placeholder="Type Message ..." class="form-control">
-								<span class="input-group-btn">
-                        <button type="submit" class="btn btn-primary btn-flat">Send</button>
-                      </span>
-							</div>
-						</form>
+						<chat-form :user=" {{ Auth::user() }}" v-on:messageSent="addMesage"></chat-form>
 					</div>
 					<!-- /.box-footer-->
 				</div>
