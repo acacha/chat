699c2975c273a66a6cccb182fea71d096e35466f
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-02-28 20:32:22 +0100

    Initial version

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+
+/**
+ * Class ChatController.
+ *
+ * @package App\Http\Controllers
+ */
+class ChatController extends Controller
+{
+    //
+    /**
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $data = [];
+        return view('chat',$data);
+    }
+
+    /**
+     * Persist message to database
+     *
+     * @param  Request $request
+     *
+     * @return array
+     */
+    public function sendMessage(Request $request)
+    {
+        $user = Auth::user();
+
+        $user->messages()->create([
+            'message' => $request->input('message')
+        ]);
+
+        return ['status' => 'Message Sent!'];
+    }
+
+}
